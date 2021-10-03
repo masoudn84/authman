6902fdec20e333bf496993cc5b5523aa976959d6
m.nikoukam <mnikoukam@gmail.com>
2021-10-03 10:42:09 -0700

    adding sqlalchemy,migrate package and mysql driver

--- authman/authman.py
+++ authman/authman.py
@@ -1,6 +1,13 @@
 from flask import Flask,Blueprint
 from flask_restful import Api
+#from flask_restx import Api  ##replace restx with restfull to document apis
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from authman.config import Config
+
+db = SQLAlchemy()
+mg = Migrate()
 
 apiv1_bp = Blueprint("apiv1",__name__,url_prefix="/api/v1")
 apiv1= Api(apiv1_bp)
@@ -10,5 +17,7 @@ from authman import resource
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config) #Load application configs.
+    db.init_app(app) # initialize sqlalchemy instance.
+    mg.init_app(app, db) #initialize database migrate instance.
     app.register_blueprint(apiv1_bp)#Load Api v1
     return app
--- authman/config/config.py
+++ authman/config/config.py
@@ -4,3 +4,8 @@ class Config:
     ENV = environ.get("FANTOM_AUTHMAN_ENV","production")
     TESTING = int(environ.get("FANTOM_AUTHMAN_TESTING","0"))
     DEBUG = int(environ.get("FANTHOM_AUTHMAN_DEBUG","0"))
+    #==================Database Config======================
+    SQLALCHEMY_DATABASE_URI = environ.get("FANTOM_AUTHMAN_DATABASE_URI", None)
+    SQLALCHEMY_ECHO = DEBUG
+    SQLALCHEMY_QUERIES = DEBUG
+    SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
