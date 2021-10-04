e23cf845bb6af2b23e1292b14f01211425c58df0
m.nikoukam <mnikoukam@gmail.com>
2021-10-04 10:36:33 -0700

    adding now function

--- authman/config/config.py
+++ authman/config/config.py
@@ -1,9 +1,10 @@
 from os import environ
 class Config:
-    #==================Global Configuration================
+    #==================Global Configuration=================
     ENV = environ.get("FANTOM_AUTHMAN_ENV","production")
     TESTING = int(environ.get("FANTOM_AUTHMAN_TESTING","0"))
     DEBUG = int(environ.get("FANTHOM_AUTHMAN_DEBUG","0"))
+    TIMEZONE = environ.get("FANTOM_AUTHMAN_TIMEZONE","Asia/Tehran")
     #==================Database Config======================
     SQLALCHEMY_DATABASE_URI = environ.get("FANTOM_AUTHMAN_DATABASE_URI", None)
     SQLALCHEMY_ECHO = DEBUG
---
+++ authman/util/now.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from pytz import timezone, UTC 
+from authman.config import Config
+
+def now(name=Config.TIMEZONE):
+    tz = timezone(name)
+    return datetime.utcnow().replace(tzinfo=utc).astimezone(tz).replace(
+        microsecond=0, tzinfo=None
+    )
