2e54fda46a0c5fbd7cff4a6fbfa68588f73d93c7
m.nikoukam <mnikoukam@gmail.com>
2021-10-07 20:47:57 +0330

    adding user expires_at function

--- authman/util/__init__.py
+++ authman/util/__init__.py
@@ -1,3 +1,4 @@
 from authman.util.jsonify import jsonify 
+from authman.util.expires import user_expires_at
 from authman.util.now import now
 from authman.util.uuidgen import uuidgen
---
+++ authman/util/expires.py
@@ -0,0 +1,7 @@
+from datetime import timedelta
+
+from authman.config import Config
+from authman.util import now
+
+def user_expires_at():
+    return now() + timedelta(days=Config.USER_DEFAULT_EXPIRY_TIME)
