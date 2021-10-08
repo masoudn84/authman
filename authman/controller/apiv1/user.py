2198eaea3195c9667612cf05fa0da155be35d7ba
m.nikoukam <mnikoukam@gmail.com>
2021-10-09 02:24:34 +0330

    import subpackages and create database initial migrate

--- authman/controller/apiv1/user.py
+++ authman/controller/apiv1/user.py
@@ -1,8 +1,14 @@
+from authman.authman import db
+from authman.model import User
+from authman.schema.apiv1 import UserSchema
 from authman.util import jsonify
 
 
 class UserController:
     def get_users():
+        users_schema = UserSchema(many=True)
+        users = User.query.all()
+        return jsonify({"Users": users_schema.dump(users)})
         return jsonify(status=501) # Not Implemented.
 
     def get_user(user_id):
