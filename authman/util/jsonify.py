5ea9bb73df01e913d476e54cdbe1d65c1f811e09
m.nikoukam <mnikoukam@gmail.com>
2021-10-09 13:49:14 +0330

    update jsonify

--- authman/util/jsonify.py
+++ authman/util/jsonify.py
@@ -1,7 +1,24 @@
 from flask import current_app
 DEBUG_MSG_CODES = {
 "100": "OK",
-"101": "Not Implemented"
+"101": "Unsupported Media Type",
+"102": "Database Error",
+"103": "Resource Not Found",
+"104": "Request Validation Faild",
+"105": "Empty Data Supplied",
+"106": "Resource Conflict",
+"107": "Not Implemented",
+"108": "Resource Expired",
+"109": "Bad Desired Status",
+"110": "Token Encryption Error",
+"111": "Resource Not Matched",
+"112": "Header Not Specified",
+"113": "Token Validation Error",
+"114": "Invalid Token Data",
+"115": "Controller Allowed Not Found",
+"116": "Resource Access Denied",
+"117": "Role Not Found",
+
 }
 
 def jsonify(state={}, metadata={},status=200,code=100,headers={}):
