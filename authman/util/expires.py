dd8d1610b6b9b0a91d4cdabf8e1619add75f25c6
m.nikoukam <mnikoukam@gmail.com>
2021-10-10 05:52:46 -0700

    edit util files and and schema for validation

--- authman/util/expires.py
+++ authman/util/expires.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
 from authman.config import Config
-from authman.util import now
+from authman.util.now import now
 
 def user_expires_at():
     return now() + timedelta(days=Config.USER_DEFAULT_EXPIRY_TIME)
