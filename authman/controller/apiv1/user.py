20cf78ee1af5f70e2f65031bfa10e6f8af8d9f76
m.nikoukam <mnikoukam@gmail.com>
2021-10-17 02:14:38 +0330

    edit uer update and delete

--- authman/controller/apiv1/user.py
+++ authman/controller/apiv1/user.py
@@ -19,30 +19,48 @@ class UserController:
         if user is None:
             return jsonify(status=404,code=103) #user not found
         return jsonify({"user": user_schema.dump(user)}) #return single user   
-        return jsonify(status=501) # Not Implementedu.
 
     def create_user():
         if not request.is_json:
             jsonify(status=415,code=101)
         user_schema = UserSchema(only=["username","password"])
         try:
-            data = user_schema.load(request.get_json())
+            data = user_schema.load(request.get_json())#json validation
         except:
             return jsonify(status=400,code=104)
         if not data["username"] or not data["password"]:
-            return jsonify(status=400, code=105)
+            return jsonify(status=400, code=105)#empty data
         user = User.query.filter_by(username=data["username"]).first()
         if user is not None:
-            return jsonify(status=400,code=106)
+            return jsonify(status=400,code=106)#user is already found
         user = User(username=data["username"], password=data["password"])
         db.session.add(user)
         db.session.commit()
         user_schema = UserSchema()
-        return jsonify({"user": user_schema.dump(user)}, status=201 )  
-        return jsonify(status=501) # Not Implementedu. 
+        return jsonify({"user": user_schema.dump(user)}, status=201)  
+         
 
     def update_user(user_id):
-        return jsonify(status=501) # Not Implemented.
-
+        if not request.is_json:
+            return jsonify(status=415,code=101)
+        user_schema = UserSchema(only=["password"])
+        try:
+            data = user_schema.load(request.get_json())
+        except:
+            return jsonify(status=400,code=104)
+        if not data["password"]:
+            return jsonify(status=400,code=105 )
+        user = User.query.get(user_id)
+        if user is None:
+            return jsonify(status=404,code=103)
+        user.password = data["password"]
+        db.session.commit()
+        user_schema = UserSchema()
+        return jsonify({"user":user_schema.dump(user)})
     def delete_user(user_id):
-        return jsonify(status=501) # Not Implemented.
+        user = User.query.get(user_id)
+        if user is None:
+            return jsonify(status=404,code=103)
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify()
